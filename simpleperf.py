001107aa6aea4c925a5232c09cb7424b0f250b77
siljeflaglien <s364579@oslomet.no>
2023-03-27 14:56:48 +0200

    Server utskrift, handle server, format choices

--- simpleperf.py
+++ simpleperf.py
@@ -1,7 +1,14 @@
 import sys 
 import argparse
 import re
+from socket import *
+import _thread as thread
 
+
+"""________________________________________________________________________________________________
+                                         CUSTOM FUNCTIONS
+    ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+"""
 # CUSTOM FUNCTIONS that need to be defined before the arguments
 
 def check_IP(val):
@@ -27,19 +34,79 @@ def check_port(val):
         sys.exit
         #Only valid port between [1204-65535]. 
 
+def check_format(val):
+    #Hvis det kommer inn i et Spesielt format, skal jeg konvertere det?
+    print
+
+
+def handleServer(port, IP):
+    serverSocket = socket(AF_INET, SOCK_STREAM) 
+    serverPort = port
+
+    try:
+        #Binding socket to a IP adress and port
+        serverSocket.bind(('IP', serverPort)) 
+    except:
+        print("Bind failed. Error : ")
+    
+    #only one can connect
+    serverSocket.listen(1) 
+    print('Server is ready to recieve')
+
+    while True:
+    #Establish the connection print('Ready to serve...') connectionSocket, addr =
+        try:
+            connectionSocket, addr = serverSocket.accept() 
+            print('Ready to serve ' , addr)
+
+        except IOError:
+            #Send response message for file not found
+            feil = "404 File Not Found"
+            connectionSocket.send(feil.encode()) 
+        
+     #Close client socket
+        connectionSocket.close()
+        serverSocket.close()
+        sys.exit()#Terminate the program after sending the corresponding data
+
+
+
+
+
+
+
+
+"""________________________________________________________________________________________________
+                                         ADDING ARGUMENTS
+    ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+"""
 
 parser=argparse.ArgumentParser(description="Portifolio 1", epilog="end of help")
 
-#Adding arguments
-# ------ Server -------
+
+# ----------------------- Server --------------------------
 parser.add_argument('-s', '--server', action='store_true') 
 parser.add_argument('-b', '--bind', type=check_IP) #input IP address
 parser.add_argument('-p','--port',type=check_port, default=8088) #input Port number
+parser.add_argument('-f','--format', type=str, choices=('B','KB','MB'))
+
+
+# ----------------------- Client --------------------------
+parser.add_argument('-c', '--client', action='store_true')
 
 
 
-# ------ Client ------
-parser.add_argument('-c', '--client', action='store_true') 
+"""________________________________________________________________________________________________
+                                         HANDLE THE ARGUMENTS
+    ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+"""
+args = parser.parse_args()
 
+if args.server:
+    print('---------------------------------------------')
+    print('A simpleperf server is listening on port '+args.port)
+    print('---------------------------------------------')
+
+    handleServer(args.port,args.bind)
 
 
