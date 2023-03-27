74e6f2b1b8ca836d94ab8ad920b7d812bb029839
siljeflaglien <s364579@oslomet.no>
2023-03-27 16:29:30 +0200

    lagt til return

--- simpleperf.py
+++ simpleperf.py
@@ -16,8 +16,9 @@ def check_IP(val):
     value = re.match('^[0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}', val)
     if not value:
         print("Not a valid IP address. I nedd to be in the format of: \n X.X.X.X \n Where X is a number between 0-127.")
-    
-    sys.exit
+        sys.exit
+    return val
+
 
 
 #Checking port in --port argument
@@ -34,6 +35,8 @@ def check_port(val):
         sys.exit
         #Only valid port between [1204-65535]. 
 
+    return value
+
 def check_format(val):
     #Hvis det kommer inn i et Spesielt format, skal jeg konvertere det?
     print
@@ -45,14 +48,15 @@ def handleServer(port, IP):
 
     try:
         #Binding socket to a IP adress and port
-        serverSocket.bind(('IP', serverPort)) 
+        serverSocket.bind((IP, serverPort)) 
     except:
         print("Bind failed. Error : ")
     
     #only one can connect
     serverSocket.listen(1) 
-    print('Server is ready to recieve')
+    print('A simpleperf client with IP address:' + str(IP)+' is connected with server IP: '+str(serverPort))
 
+    sys.exit()
     while True:
     #Establish the connection print('Ready to serve...') connectionSocket, addr =
         try:
@@ -68,6 +72,8 @@ def handleServer(port, IP):
         connectionSocket.close()
         serverSocket.close()
         sys.exit()#Terminate the program after sending the corresponding data
+    serverSocket.close()
+    sys.exit()#Terminate the program after sending the corresponding data
 
 
 
@@ -86,7 +92,7 @@ parser=argparse.ArgumentParser(description="Portifolio 1", epilog="end of help")
 
 # ----------------------- Server --------------------------
 parser.add_argument('-s', '--server', action='store_true') 
-parser.add_argument('-b', '--bind', type=check_IP) #input IP address
+parser.add_argument('-b','--bind',type=check_IP, default='10.0.0.2') #input IP address
 parser.add_argument('-p','--port',type=check_port, default=8088) #input Port number
 parser.add_argument('-f','--format', type=str, choices=('B','KB','MB'))
 
@@ -104,9 +110,12 @@ args = parser.parse_args()
 
 if args.server:
     print('---------------------------------------------')
-    print('A simpleperf server is listening on port '+args.port)
+    print('A simpleperf server is listening on port '+ str(args.port))
     print('---------------------------------------------')
 
+    print('bind: '+str(args.bind))
+    print('port: '+str(args.port))
     handleServer(args.port,args.bind)
+    
 
-
+sys.exit
