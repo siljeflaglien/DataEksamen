fe0ab91a60f9d9818554196b9ae577b624eab56e
siljeflaglien <s364579@oslomet.no>
2023-03-28 17:31:28 +0200

    check_format + printing in wanted format Har laged check_format som også konverterer til ønsket format. Den returnerer verdien etter den har blitt konvertert til riktig format. Har fikset utskriving av resultat på client og server. På begge sider så blir det skrevet ut med ønsket format.

--- simpleperf.py
+++ simpleperf.py
@@ -42,9 +42,15 @@ def check_port(val):
     return value #Returning the vallue because it was valid and so that it can be used
 
 #Checking format in --format argument
-def check_format(val):
+def check_format(val, format):
     #Hvis det kommer inn i et Spesielt format, skal jeg konvertere det?
-    print
+    if val == 0 or format == 'B':
+        return val
+    elif format == 'MB':
+        return val/1000000
+    elif format == 'KB':
+        return val/1000
+
 
 #Checking seconds in --time argument
 def check_time(val):
@@ -78,7 +84,7 @@ def check_num_conn(val):
                                          HANDLE SERVER
     ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
 """
-def handleServer(port, IP):
+def handleServer(port, IP, format):
     serverSocket = socket(AF_INET, SOCK_STREAM) 
     serverPort = port #port number of the server
     datareceived=0  #will get updated each time i receive data
@@ -128,24 +134,26 @@ def handleServer(port, IP):
                     #If not BYE, we add it to how much data we have received
                     datareceived+=getsizeof(message)
             
-            
-            receivedMB = datareceived/1000000 # from B -> MB
+             # ----------------------- PRINTING RESULTS ----------------------------
+            receivedMB = check_format(datareceived, 'MB') # from B -> MB
             print('received B: '+str(datareceived))
             print('received MB: '+str(receivedMB))
             print('end: '+str(end))
             rate = receivedMB/end #calculating the rate
 
+            transferformat=check_format(datareceived,format) #Changing to the chosen format from --format
+
             #sets them to only 2 decimals
             rate = '{0:.2f}'.format(rate)
-            receivedMB = '{0:.2f}'.format(receivedMB)
+            transferformat = '{0:.2f}'.format(transferformat)
            
             #Printig out "IP, Interval, Received and Rate" table
             print()
-            d = {str(IP)+":"+ str(port):["0.0 - "+str(rectime)+'.0', str(receivedMB)+' MB' , str(rate)+' Mbps']}
-            print ("{:<15} {:<12} {:<13} {:<10}".format('ID','Interval','Received','Rate'))
+            d = {str(IP)+":"+ str(port):["0.0 - "+str(rectime)+'.0', str(transferformat)+' '+format , str(rate)+' Mbps']}
+            print ("{:<15} {:<12} {:<17} {:<10}".format('ID','Interval','Received','Rate'))
             for k, v in d.items():
                 lang, perc, change = v
-                print ("{:<15} {:<12} {:<13} {:<10}".format(k, lang, perc, change))
+                print ("{:<15} {:<12} {:<17} {:<10}".format(k, lang, perc, change))
             print()
             #End of printing table
 
@@ -164,14 +172,15 @@ def handleServer(port, IP):
                                          HANDLE CLIENT
     ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
 """
-def handleClient(serverIP,port, sendtime):
+def handleClient(serverIP,port, sendtime, format):
     socketClient = socket(AF_INET, SOCK_STREAM) #Creating socket for client
     clientPort = port #Port number of the server
     host = serverIP #IP address of the server
     bye = 'BYE' #for when we send the bye message
+    sizesent=0 #sizesent
 
     #Making a data packet with 1000bytes
-    data='0'*951 #the byte size will be 1000 bytes with 951
+    data='0'*1000 #the byte size will be 1000 bytes with 951
    
     print(str(getsizeof(data))+' bytes') #prints 1000 bytes (how many bytes the data packet is)
     #print(data)
@@ -193,10 +202,13 @@ def handleClient(serverIP,port, sendtime):
 
     while (time.time() < t+sendtime): #sending it for "sendtime"-amount of seonds
         socketClient.send(data.encode()) #Sending the data to the server
+        sizesent+=getsizeof(data)
     
     sendtime=str(sendtime) #changing the seconds to a String to make it easier further down
     print(sendtime+' sekunder har gått')
 
+    end=time.time() #time finished sending packets
+    
     time.sleep(0.3) #To separate BYE and datapackets so they dont get sendt in the same message
 
     socketClient.send(bye.encode()) #Sends BYE message
@@ -209,7 +221,33 @@ def handleClient(serverIP,port, sendtime):
 
         #Closing and exiing 
         socketClient.close() 
-        exit(1)
+    
+
+    # --------------------- PRINTING RESULTS --------------------------
+    #Calculating bandwidth
+    totaltduration=end-t #gives totalt duration of sending
+    sizeMB=check_format(sizesent,'MB') #Gives the size sent in MB instead of B
+    bandwidth=sizeMB/totaltduration #The bandwith calculated in Mbps
+    print('Sizesendt: '+ str(sizesent)+' B\nSize MB: '+str(sizeMB)+' MB')
+
+    transferformat=check_format(sizesent,format) #Changing to the chosen format from --format
+    print('Sizesendt: '+ str(sizesent)+' B\nSize MB: '+str(sizeMB)+' MB\nSize KB: '+str(transferformat))
+
+    #sets them to only 2 decimals
+    bandwidth = '{0:.2f}'.format(bandwidth)
+    transferformat = '{0:.2f}'.format(transferformat)
+
+    #Printig out "IP, Interval, Transfer and Bandwisth" table
+    print()
+    d = {str(serverIP)+":"+ str(port):["0.0 - "+str(sendtime)+'.0', str(transferformat)+' '+format , str(bandwidth)+' Mbps']}
+    print ("{:<15} {:<12} {:<17} {:<10}".format('ID','Interval','Transfer','Bandwith'))
+    for k, v in d.items():
+        lang, perc, change = v
+        print ("{:<15} {:<12} {:<17} {:<10}".format(k, lang, perc, change))
+    print()
+    #End of printing table
+
+    exit(1)
 
 
 
@@ -269,12 +307,12 @@ elif args.server:
 
     print('bind: '+str(args.bind))
     print('port: '+str(args.port))
-    handleServer(args.port,args.bind)
+    handleServer(args.port,args.bind,args.format)
     
 elif args.client:
     print('------------------------------------------------------------------------------------------')
     print('A simpleperf client connecting to server '+str(args.serverip)+', port '+str(args.port))
     print('------------------------------------------------------------------------------------------')
-    handleClient(args.serverip,args.port, int(args.time))
+    handleClient(args.serverip,args.port, int(args.time), args.format)
 
 sys.exit
